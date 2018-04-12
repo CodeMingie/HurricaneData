51cd85fbb0cd4b7a79b6e37baab0571418668b94
Ming Luo <jiamingluo123@gmail.com>
2018-04-11 22:10:36 -0400

    Add try catch

--- HurDatForms/Form1.cs
+++ HurDatForms/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,166 @@ namespace HurDatForms
 {
     public partial class Form1 : Form
     {
+        private List<Hur> hurList;
+        private string hurDatFile = @"Files/hurdat2-1851-2016-041117.txt";
+
         public Form1()
         {
             InitializeComponent();
+
+            gridHur.ReadOnly = true;
+            gridHurDetail.ReadOnly = true;
+
+            List<string> states = new List<string>();
+            states.Add(string.Empty);
+            states.AddRange(new StateLookup().GetStates());
+            stateDropdown.DataSource = states;
+        }
+
+        //Update hurricane detail if selected hurricane changes
+        private void gridHur_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            { 
+                Hur hur = (Hur) gridHur.Rows[e.RowIndex].DataBoundItem;
+                gridHurDetail.DataSource = hur.Details;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Filter list of models using LINQ
+        private void filter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (hurList == null)
+                {
+                    MessageBox.Show("Load data first.");
+                    return;
+                }
+
+                if (recordText.Text.Length > 1)
+                { 
+                    MessageBox.Show("Record Identifier must be 1 character.");
+                    return;
+                }
+
+                List<Hur> filteredHur = hurList;
+                string state = (string)stateDropdown.SelectedValue;
+
+                //If both state and recordId are set, then filter any hurricane detail on state AND recordId
+                if (!string.IsNullOrEmpty(state) && recordText.Text.Length > 0)
+                {
+                    filteredHur = filteredHur.Where(h => h.Details.Any(d => d.State == state && d.RecordId == recordText.Text)).ToList();
+                    foreach (Hur hur in filteredHur)
+                    {
+                        hur.Details = hur.Details.Where(d => d.State == state && d.RecordId == recordText.Text).ToList();
+                    }
+                }
+                else if (recordText.Text.Length > 0)
+                {
+                    filteredHur = filteredHur.Where(h => h.Details.Any(d => d.RecordId == recordText.Text)).ToList();
+                    foreach (Hur hur in filteredHur)
+                    {
+                        hur.Details = hur.Details.Where(d => d.RecordId == recordText.Text).ToList();
+                    }
+                }
+                else if (!string.IsNullOrEmpty(state))
+                {
+                    filteredHur = filteredHur.Where(h => h.Details.Any(d => d.State == state)).ToList();
+                    foreach (Hur hur in filteredHur)
+                    {
+                        hur.Details = hur.Details.Where(d => d.State == state).ToList();
+                    }
+                }
+
+                int minYear;
+                if (minYearText.Text.Length > 0)
+                {
+                    if (!int.TryParse(minYearText.Text, out minYear))
+                        MessageBox.Show("Invalid Min Year.");
+                    else
+                    {
+                        filteredHur = filteredHur.Where(h => h.Year >= minYear).ToList();
+                    }
+                }
+
+                this.numText.Text = filteredHur.Count.ToString();
+                gridHur.DataSource = filteredHur;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Parse data and load into list of models
+        private void loadData_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                HurDatParser p = new HurDatParser(hurDatFile);
+                string errors;
+                List<Hur> results = p.ParseHurDat(out errors);
+
+                if (!string.IsNullOrEmpty(errors))
+                { 
+                    MessageBox.Show(errors);
+                    return;
+                }
+
+                if (results == null)
+                { 
+                    MessageBox.Show("ParseHurDat returned null");
+                    return;
+                }
+
+                hurList = results;
+                gridHur.DataSource = hurList;
+                this.numText.Text = hurList.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Print out the hurricane name, date and max wind as per requirements
+        private void report_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                List<Hur> filteredHur = (List<Hur>) gridHur.DataSource;
+
+                if (filteredHur == null)
+                {
+                    MessageBox.Show("Load data first.");
+                    return;
+                }
+
+                StringBuilder csv = new StringBuilder();
+                foreach (Hur h in filteredHur)
+                {
+                    foreach (HurDetail d in h.Details)
+                    {
+                        DateTime date = new DateTime(d.Year, d.Month, d.Day);
+                        string strDate = date.ToString("MM/dd/yyyy");
+                        string line = string.Format("{0}, {1}, {2}", h.Name, strDate, d.MaxWind);
+                        csv.AppendLine(line);
+                    }
+                }
+
+                string fileName = "hurdatareport" + DateTime.Now.ToString("MMddyy_HHmmss") + ".csv";
+                File.WriteAllText(fileName, csv.ToString());
+                Process.Start(fileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
---
+++ HurDatForms/HurDatParser.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HurDatForms
+{
+    public class HurDatParser
+    {
+        string hurdatFile;
+
+        public HurDatParser(string file)
+        {
+            hurdatFile = file;
+        }
+
+        //Validate data file, outputing any errors to console. Return true if no error.
+        private bool ValidateHurDat(out string errors)
+        {
+            StringBuilder errorBuilder = new StringBuilder();
+            
+            if (!File.Exists(hurdatFile))
+                errorBuilder.AppendLine("File does not exist.");
+
+            string line;
+            int lineNum = 0;
+            
+            System.IO.StreamReader file = new System.IO.StreamReader(hurdatFile);
+            int expectedEntries = -1;
+            int currentEntries = 0;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                lineNum++;
+                string[] blocks = line.Split(',');
+
+                //Header line
+                if (blocks.Length == 4)
+                {
+                    if (blocks[0].Length != 8)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Not 8 characters");
+
+                    string basin = blocks[0].Substring(0, 2);
+                    string strCycloneNum = blocks[0].Substring(2, 2);
+                    string strYear = blocks[0].Substring(4, 4);
+
+                    int cycloneNum;
+                    int year;
+
+                    if (!int.TryParse(strCycloneNum, out cycloneNum))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse cyclone number");
+
+                    if (!int.TryParse(strYear, out year))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse year");
+
+                    string hurName = blocks[1];
+
+                    string strTrackEntries = blocks[2];
+                    int trackEntries;
+
+                    if (!int.TryParse(strTrackEntries, out trackEntries))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse track entries");
+
+                    if (expectedEntries == -1)
+                    {
+                        expectedEntries = trackEntries;
+                        continue;
+                    }
+
+                    if (expectedEntries != currentEntries)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Different from expected Entries");
+
+                    expectedEntries = trackEntries;
+                    currentEntries = 0;
+                }
+                //Data line
+                else if (blocks.Length == 21)
+                {
+                    currentEntries++;
+                    if (blocks[0].Length != 8)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Not 8 characters");
+
+                    string strYear = blocks[0].Substring(0, 4);
+                    string strMonth = blocks[0].Substring(4, 2);
+                    string strDay = blocks[0].Substring(6, 2);
+
+                    int year;
+                    int month;
+                    int day;
+
+                    if (!int.TryParse(strYear, out year))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse year");
+
+                    if (!int.TryParse(strMonth, out month))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse month");
+
+                    if (!int.TryParse(strDay, out day))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse day");
+
+                    if (blocks[1].Trim().Length != 4)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Data line time not 4 characters");
+
+                    string strHour = blocks[0].Substring(0, 2);
+                    string strMinutes = blocks[0].Substring(2, 2);
+
+                    int hour;
+                    int minutes;
+
+                    if (!int.TryParse(strHour, out hour))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse hour");
+
+                    if (!int.TryParse(strMinutes, out minutes))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse minute");
+
+                    string recordId = blocks[2].Trim();
+
+                    if (recordId.Length > 1)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", RecordId more than length 1");
+
+                    string status = blocks[3].Trim();
+
+                    if (status.Length > 2)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Status more than length 2");
+
+
+                    if (blocks[4].Trim().Length < 4)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Invalid latitude length");
+
+                    string strLat = blocks[4].Substring(0, blocks[4].Length - 1).Trim();
+                    char latHemi = blocks[4][blocks[4].Length - 1];
+
+                    if (latHemi != 'S' && latHemi != 'N')
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Invalid latitude hemisphere");
+
+                    decimal lat;
+
+                    if (!decimal.TryParse(strLat, out lat))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse latitude");
+
+                    if (lat > 90)
+                        errorBuilder.Append(lineNum.ToString() + ", Latitude exceeds 90.");
+
+                    if (blocks[5].Trim().Length < 4)
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Invalid longitude length");
+
+                    string strLng = blocks[5].Substring(0, blocks[5].Length - 1).Trim();
+                    char lngHemi = blocks[5][blocks[5].Length - 1];
+
+                    if (lngHemi != 'W' && lngHemi != 'E')
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Invalid longitude hemisphere");
+
+                    decimal lng;
+
+                    if (!decimal.TryParse(strLng, out lng))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse longitude");
+
+                    //if (lng > 180)
+                        //errorBuilder.AppendLine(lineNum.ToString() + ", Longitude exceeds 180");
+
+                    string strMaxWind = blocks[6].Trim();
+                    int maxWind;
+                    if (!int.TryParse(strMaxWind, out maxWind))
+                        errorBuilder.AppendLine(lineNum.ToString() + ", Cannot parse max wind");
+                }
+                else
+                {
+                    errorBuilder.AppendLine(lineNum.ToString() + ", Invalid number of columns");
+                }
+            }
+
+            if (expectedEntries != currentEntries)
+                errorBuilder.AppendLine(lineNum.ToString() + ", Different from expected Entries");
+
+            file.Close();
+
+            //if (errorBuilder.Length > 0)
+            //    Console.Out.WriteLine(errorBuilder.ToString());
+            errors = errorBuilder.ToString();
+
+            return errorBuilder.Length == 0;
+        }
+
+        public List<Hur> ParseHurDat(out string errors)
+        {
+            if (!ValidateHurDat(out errors))
+                return null;
+
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(hurdatFile);
+            List<Hur> hurList = new List<Hur>();
+            int hurId = 0;
+            Hur currentHur = new Hur();
+            StateLookup sl = new StateLookup();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] blocks = line.Split(',');
+                
+                //Header line
+                if (blocks.Length == 4)
+                {
+                    string basin = blocks[0].Substring(0, 2);
+                    string strCycloneNum = blocks[0].Substring(2, 2);
+                    string strYear = blocks[0].Substring(4, 4);
+
+                    int cycloneNum;
+                    int year;
+
+                    int.TryParse(strCycloneNum, out cycloneNum);
+                    int.TryParse(strYear, out year);
+                    string hurName = blocks[1].Trim();
+                    string strTrackEntries = blocks[2];
+                    int trackEntries;
+                    int.TryParse(strTrackEntries, out trackEntries);
+
+                    hurId++;
+
+                    Hur hur = new Hur();
+                    hur.Id = hurId;
+                    hur.Basin = basin;
+                    hur.CycloneNum = cycloneNum;
+                    hur.Year = year;
+                    hur.Name = hurName;
+                    hur.NumEntries = trackEntries;
+                    hur.Details = new List<HurDetail>();
+                    hurList.Add(hur);
+                    currentHur = hur;
+                }
+                //Data line
+                else if (blocks.Length == 21)
+                {
+                    string strYear = blocks[0].Substring(0, 4);
+                    string strMonth = blocks[0].Substring(4, 2);
+                    string strDay = blocks[0].Substring(6, 2);
+
+                    int year;
+                    int month;
+                    int day;
+
+                    int.TryParse(strYear, out year);
+                    int.TryParse(strMonth, out month);
+                    int.TryParse(strDay, out day);
+
+                    string strHour = blocks[0].Substring(0, 2);
+                    string strMinutes = blocks[0].Substring(2, 2);
+
+                    int hour;
+                    int minutes;
+
+                    int.TryParse(strHour, out hour);
+                    int.TryParse(strMinutes, out minutes);
+
+                    string recordId = blocks[2].Trim();
+                    string status = blocks[3].Trim();
+
+                    string strLat = blocks[4].Substring(0, blocks[4].Length - 1).Trim();
+                    char latHemi = blocks[4][blocks[4].Length - 1];
+
+                    decimal lat;
+                    decimal.TryParse(strLat, out lat);
+
+                    if (latHemi == 'S')
+                        lat = lat * -1;
+
+                    string strLng = blocks[5].Substring(0, blocks[5].Length - 1).Trim();
+                    char lngHemi = blocks[5][blocks[5].Length - 1];
+
+                    decimal lng;
+                    decimal.TryParse(strLng, out lng);
+
+                    if (lngHemi == 'W')
+                        lng = lng * -1;
+
+                    string strMaxWind = blocks[6].Trim();
+                    int maxWind;
+                    int.TryParse(strMaxWind, out maxWind);
+
+                    HurDetail detail = new HurDetail();
+                    detail.HurId = currentHur.Id;
+                    detail.Year = year;
+                    detail.Month = month;
+                    detail.Day = day;
+                    detail.Hour = hour;
+                    detail.Minute = minutes;
+                    detail.RecordId = recordId;
+                    detail.Status = status;
+                    detail.Latitude = lat;
+                    detail.Longtitude = lng;
+                    detail.MaxWind = maxWind;
+                    detail.State = sl.GetStateFromCor(lng, lat);
+
+                    currentHur.Details.Add(detail);
+                }
+            }
+
+            file.Close();
+
+            return hurList;
+        }
+    }
+}
